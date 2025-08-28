289bfc86bd697bc463d3df5bf0fe21e847db067d
harshal1610-97 <harshalbansode04@gmail.com>
2025-08-29 00:36:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Harshal Sharad Bansode",
+    author_email="harshalbansode04@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
